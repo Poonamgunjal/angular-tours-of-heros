f88981c9e74fae19966c768eee7bb00b55d423f9
poonam <poonam.gunjal@clairvoyant.com>
2017-03-06 10:02:44 +0530

    initial commit for tour of heros

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero='Windstorm';
+
+
+
+}
+export class Hero {
+  id: number;
+  name: string;
+}
+
+hero: Hero = {
+  id: 1,
+  name: 'Windstorm'
+};
